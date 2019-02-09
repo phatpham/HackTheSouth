feaf5eaa8364229d68b7b0f0cd6e8c5dc4012af5
samjones246 <sam.jones240699@gmail.com>
2019-02-09 14:48:44 +0000

    preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+def binarizeAndInvert(img):
+    # Otsu's thresholding after Gaussian filtering
+    blur = cv.GaussianBlur(img,(5,5),0)
+    _,out = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    out = cv.bitwise_not(out)
+    return out
